2a1125ed3083dba00ef13a6a493bcaa6ecf2fb94
Marlom <marlomsilvajob@gmail.com>
2024-05-20 19:44:13 -0300

    Feito botão de empréstimo e form filho

---
+++ Bibliotec-Desenvolvimento/Bibliotec/Forms/Emprestimo.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Bibliotec.Forms
+{
+    partial class Emprestimo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(511, 93);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Marlom";
+            label1.Click += label1_Click;
+            // 
+            // Emprestimo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1216, 631);
+            Controls.Add(label1);
+            Name = "Emprestimo";
+            Text = "Emprestimo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
